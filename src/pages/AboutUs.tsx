5d86ba088fe2504f3d15e1c135e6e53a59759d4f
AZ-007 <aboodi-75-@hotmail.com>
2024-05-28 11:57:10 +0300

    fix aboutUs bug

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,7 +1,6 @@
 import NavBar from "@/components/NavigationBar";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import videoH from "../assets/Hero Image.jpg"
 
 export default function AboutUs() {
   return (
